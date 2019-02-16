1a0499fb06d10b2a789655fbddc6dc448142660c
deanmixer <deanmixer@users.noreply.github.com>
2019-02-16 13:40:23 -0500

    Page update

--- assets/scripts/ajaxCall.js
+++ assets/scripts/ajaxCall.js
@@ -1,1 +1,17 @@
 //code here
+
+$("button").click(function(){
+    $.post("/api/viewtable",
+    {
+      "name": $("#tableName").val().trim(),
+      "phone": $("phoneNumber").val().trim(), 
+      "email": $("inputEmail1").val().trim(),
+      "id": $("tableId").val().trim()
+    },
+    function(data, status){
+      if (data == true) {
+        alert("Your reservation has been confirmed!")
+      } else alert("Our tables are booked for tonight, but you've been added to the waitlist")
+    }
+    );
+  });
