d59216ebf3a97354a943c402d56be148b018c6ae
ukgt <ukgt2019@gmail.com>
2019-02-16 13:37:28 -0500

    added logic for server side

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use("/assets", express.static(__dirname + "/assets"));
 //  customers (DATA)
 // =============================================================
 var customers = [
@@ -73,6 +73,7 @@ app.get("/api/viewwaitlist", function(req, res) {
 
 
 
+
 // Create New customers - takes in JSON input
 app.post("/api/viewtable", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
@@ -84,27 +85,16 @@ app.post("/api/viewtable", function(req, res) {
   newcustomer.routeName = newcustomer.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcustomer);
-
+if(customers.length < 5){
   customers.push(newcustomer);
+  res.json(true);
+}else{
+  waitList.push(newcustomer);
+}
 
-  res.json(newcustomer);
 });
-// for the wait list:
-app.post("/api/viewwaitlist", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcustomer = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newcustomer
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcustomer.routeName = newcustomer.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newcustomer);
-
-  customers.push(newcustomer);
-
-  res.json(newcustomer);
-});
 
 // Starts the server to begin listening
 // =============================================================
